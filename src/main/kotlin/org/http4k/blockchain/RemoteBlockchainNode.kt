031ebfd43154195e4b75477d15a3dee9a3230c76
David Denton <denton.david@gmail.com>
2017-10-01 22:25:20 +0100

    spiking out a blockchain

--- src/main/kotlin/org/http4k/blockchain/RemoteBlockchainNode.kt
+++ src/main/kotlin/org/http4k/blockchain/RemoteBlockchainNode.kt
@@ -1,19 +1,17 @@
 package org.http4k.blockchain
 
+import org.http4k.client.ApacheClient
 import org.http4k.core.Body
-import org.http4k.core.HttpHandler
-import org.http4k.core.Method
+import org.http4k.core.Method.GET
 import org.http4k.core.Request
-import org.http4k.core.Response
-import org.http4k.core.Status
 import org.http4k.core.Uri
 import org.http4k.format.Jackson.auto
 
 class RemoteBlockchainNode(val address: Uri) : BlockchainNode {
-    private val client: HttpHandler = { Response(Status.OK) }
+    private val client = ApacheClient()
 
     private val chain = Body.auto<List<Block>>().toLens()
 
     override fun chain(): List<Block> =
-        chain.extract(client(Request(Method.GET, address.path("/chain"))))
+        chain.extract(client(Request(GET, address.path("/chain"))))
 }
