e1f17e7957b79f64b9b03c24a014b03e2ff71790
David Denton <denton.david@gmail.com>
2017-10-03 12:58:37 +0100

    extracted Stack and NodeApi

--- src/main/kotlin/org/http4k/blockchain/node/RemoteNode.kt
+++ src/main/kotlin/org/http4k/blockchain/node/RemoteNode.kt
@@ -1,6 +1,5 @@
 package org.http4k.blockchain.node
 
-import org.http4k.blockchain.Block
 import org.http4k.blockchain.Protocol
 import org.http4k.blockchain.Protocol.block
 import org.http4k.blockchain.Protocol.chain
@@ -17,7 +16,7 @@ class RemoteNode(override val address: Uri) : Node {
 
     override fun chain() = chain.extract(client(Request(GET, "/chain")))
 
-    fun mine(): Block = block.extract(client(Request(GET, "/mine")))
+    fun mine() = block.extract(client(Request(GET, "/mine")))
 
     override fun newTransaction(newTransaction: Transaction) {
         client(Request(Method.POST, "/transactions")
